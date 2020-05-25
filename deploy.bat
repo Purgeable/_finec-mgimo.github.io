781864cf81213830d258296af37549fccf282575
Evgeniy <e.pogrebnyak@gmail.com>
2020-05-25 10:17:28 +0300

    Deployed d1ab4a9 with MkDocs version: 1.1.2

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-REM Workflow described here: https://www.mkdocs.org/user-guide/deploying-your-docs/
-REM Keep in mind: github org pages serve from master branch only
-REM We currently use . folder and  ../finec/mkdocs.yml
-mkdocs gh-deploy --config-file ../finec/mkdocs.yml --remote-branch master
-
-REM Note mkdocs is not good at supporting two languages, may need more repos for that.
